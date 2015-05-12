efa9324975651b7d6ff11814568f51a21aec89f0
jroca72 <jroca72@gmail.com>
2015-05-12 20:34:25 +0200

    carpetas, index, base, css y más

--- peregrinos/clinica/views.py
+++ peregrinos/clinica/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse,HttpResponseRedirect
+from django.template import RequestContext
+
+from clinica.models import clientes, historial, citas
 
 # Create your views here.
+
+def index(request):
+	las_citas = citas.objects.all()
+	context = {'las_citas': las_citas }
+	return render(request, 'clinica/index.html', context)
+
+#--------------------------------------------------------------------	
