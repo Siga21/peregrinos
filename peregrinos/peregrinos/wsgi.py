8ef154ca6239f853dadc837174245fee9c0685c3
root <root@siga21server.home>
2015-06-25 20:19:55 +0200

    adaptada al servidor

--- peregrinos/peregrinos/wsgi.py
+++ peregrinos/peregrinos/wsgi.py
@@ -7,7 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
+import os,sys
+
+sys.path.append("/home/roca67/peregrinos/peregrinos")
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'indice.settings'
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "peregrinos.settings")
 
 from django.core.wsgi import get_wsgi_application
